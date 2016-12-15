ce4114502a267a122601fd5401662b60068a5e2e
Unknown <abirami96@gmail.com>
2016-12-16 02:20:04 +0800

    Add TileEngine

---
+++ DiamondHunter/src/com/neet/DiamondHunter/TileEngine/Engine.java
@@ -0,0 +1,9 @@
+package TileEngine;
+
+//hi stupid1
+
+public class Engine {
+
+		public static final int TILE_HEIGHT = 16;	//represents y variable
+		public static final int TILE_WIDTH = 16;	//represents x variable
+}
