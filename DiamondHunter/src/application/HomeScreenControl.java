706a551ac3f2ecff710ddc46758a172f288cbe95
Unknown <abirami96@gmail.com>
2016-12-18 05:40:47 +0800

    Creating contoller class for EditScreen and changing GUI

--- DiamondHunter/src/application/HomeScreenControl.java
+++ DiamondHunter/src/application/HomeScreenControl.java
@@ -3,20 +3,41 @@
 package application;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import com.neet.DiamondHunter.Main.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class HomeScreenControl {	
+import java.awt.Graphics;
+import java.io.IOException;
 
-	@FXML public void onClickPlay(){	//calls this function when Play Game button in GUI is clicked
+import com.neet.DiamondHunter.TileEngine.PanelGraphics;
+
+//import com.neet.DiamondHunter.Main.*;
+
+public class HomeScreenControl extends Pane {	
+
+	PanelGraphics display = new PanelGraphics();
+	
+	@FXML public void onClickPlay(){	//calls this method when Play Game button in GUI is clicked
 	}
 	
-	@FXML public void onClickView(){	//calls this function when View Map button in GUI is clicked
-		
+	@FXML public void onClickView(){	//calls this method when View Map button in GUI is clicked
+		Graphics g = null;
+		display.paintComponent(g);
 	}
 	
-	@FXML public void onClickEdit(){	//calls this function when Edit Position button in GUI is clicked
-		
+	@FXML public void onClickEdit() throws IOException{	//calls this method when Edit Position button in GUI is clicked
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EditScreenControl.fxml"));
+		Parent root = fxmlLoader.load();
+		Stage stage = new Stage();
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.setOpacity(1);
+		stage.setTitle("My New Stage Title");
+		stage.setScene(new Scene(root, 450, 450));
+		stage.showAndWait();
 	}
 	
 }
