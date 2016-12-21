7eea2495de1c7ea23033e16eb1d34180907fe510
Oumme <oumme.kootab@hotmail.com>
2016-12-21 10:31:34 +0400

    Edited Controller Class
    Added fully-functioning Show Map button on GUI

--- DiamondHunter/src/application/Controller.java
+++ DiamondHunter/src/application/Controller.java
@@ -2,6 +2,7 @@ package application;
 
 
 import com.neet.DiamondHunter.Main.Game;
+import com.neet.DiamondHunter.TileEngine.*;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,13 +12,13 @@ public class Controller {
 	@FXML private Button btnPlay;
 	
 	 @FXML protected void Play(ActionEvent event) {
-	        //System.out.println("Sign in button pressed");
+	     
 		 	Game.main(null);
 	    }
 	 
 	 @FXML private Button btnShow;
 	 @FXML protected void ShowMap(ActionEvent event) {
-	        System.out.println("Sign in button pressed");
-		 	//Game.main(null);
+	        
+			com.neet.DiamondHunter.TileEngine.Main.main(null);
 	    }
 }
---
+++ DiamondHunter/src/com/neet/DiamondHunter/TileEngine/Engine.java
@@ -0,0 +1,8 @@
+package com.neet.DiamondHunter.TileEngine;
+
+
+public class Engine {
+
+		public static final int TILE_HEIGHT = 16;	//represents y variable
+		public static final int TILE_WIDTH = 16;	//represents x variable
+}
